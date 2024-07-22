fb308eb9491c8bf875a298832ad4f489338c9679
Himanshu kumar <128959804+imhimanshukumar@users.noreply.github.com>
2024-07-22 20:58:45 +0530

    Update youtube.js

--- src/constant/youtube.js
+++ src/constant/youtube.js
@@ -1,4 +1,4 @@
-// named export 
+
 export const API_KEY = "AIzaSyC1YCNWBccXY2zHxqz9bATc5h9ztFH4Bb4"; 
 export const BASE_URL = "https://www.googleapis.com/youtube/v3"
 export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`;
